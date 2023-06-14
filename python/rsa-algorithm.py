57171102ada5f7392c5955f28011facdd631496f
patcharachart jirasrisopa <few@patcharacharts-MacBook-Air.local>
2023-06-15 01:09:50 +0700

    rsa-algorithm example for generate public key and private key using in https

--- python/rsa-algorithm.py
+++ python/rsa-algorithm.py
@@ -14,26 +14,32 @@ q = 7
 n = p * q
 e = 2
 z = (p - 1) * (q - 1)
+print("z=", z)
+if e > 1 and e < z:
 
-while e < z:
-    if gcd(e, z) == 1:
-        break
+
+    while e < z:
+        if gcd(e, z) == 1:
+            break
+        else:
+            e = e + 1
+
+    k = 2
+    d = ((k * z) + 1) / e
+
+    print("n=%f\ne=%f\nd=%f" % (n, e, d))
+
+    painText = 12.0
+    if painText >= 0 and painText < n:
+        print("painText=", painText)
+
+        encrypted = math.fmod(pow(painText, e), n)
+        print("encrypted=", encrypted)
+
+        decrypted = math.fmod(pow(encrypted, d), n)
+        print("decrypted=", decrypted)
     else:
-        e = e + 1
+        print("function not supported this painText")
 
-k = 2
-d = ((k * z) + 1) / e
-
-print("n=%f\ne=%f\nd=%f" % (n, e, d))
-
-painText = 12.0
-if painText >= 0 and painText < n:
-    print("painText=", painText)
-
-    encrypted = math.fmod(pow(painText, e), n)
-    print("encrypted=", encrypted)
-
-    decrypted = math.fmod(pow(encrypted, d), n)
-    print("decrypted=", decrypted)
 else:
-    print("function not supported this painText")
+    print("value e not supported")
