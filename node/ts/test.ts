6466949c93d1065102354b8315380ff672c3e24b
patcharachart jirasrisopa <patcharachart@odds.team>
2023-08-24 18:13:56 +0700

    [UPDATE] tdd

--- node/ts/test.ts
+++ node/ts/test.ts
@@ -2,8 +2,38 @@
 //     return a + b
 // }
 
-// console.log(muti(1, 2));
-const x = undefined
-console.log(typeof x === 'undefined');
+import { log } from "console"
 
-console.log(x);
+// console.log(muti(1, 2));
+// const x = ''
+// console.log(typeof x === 'string');
+// const customerName = 'few za'
+// const [ firstName, lastName ] = customerName.split(' ')
+// const lastName = customerName?.substring(1, customerName?.indexOf(' '));
+// console.log(firstName, lastName);
+
+// const time = '11/09/2023 00.00.00'.split(' ')
+// console.log(time[0])
+
+async function getContractRequest() {
+    const isLoading = false;
+    if (isLoading) {
+        return 'x'
+    }
+    return 'y'
+}
+
+async function onNext() {
+    getContractRequest()
+        .then((text) => {
+            console.log("then", text)
+        })
+        .catch((text) => {
+            console.log("catch", text)
+        })
+        .finally(() => {
+            console.log("finally")
+        })
+}
+
+onNext();
